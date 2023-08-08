b8b99b1327d8f25c475f0da4abd86dec3d5b343a
Eugene <109324479+eugenieseo16@users.noreply.github.com>
2023-08-09 02:16:55 +0900

    [Silver III] Title: 소수 구하기, Time: 5476 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,10 @@
+N, M = map(int, input().split())
+for i in range(N, M + 1):
+    if i == 1:
+        continue
+
+    for j in range(2, int(i**0.5) + 1):
+        if i % j == 0:
+            break
+    else:
+        print(i)
