5d9b6ea9b4d3afd7d85c248a7cf31b7c1ee0c0d0
Eugene <109324479+eugenieseo16@users.noreply.github.com>
2023-09-29 21:38:19 +0900

    [Bronze V] Title: 카드 게임, Time: 204 ms, Memory: 17716 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,18 @@
+// import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int ans = 0;
+        for(int i = 0;i <5;i++){
+            int number = sc.nextInt();
+            ans += number;
+        }
+        System.out.println(ans);
+    }
+}
+
+
+
+
+
