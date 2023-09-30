56c6324ffc1de527cbddc09ed874a277f700b349
Eugene <109324479+eugenieseo16@users.noreply.github.com>
2023-09-30 23:54:11 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 204 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.java
@@ -0,0 +1,17 @@
+// import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       int num = sc.nextInt();
+       num ++;
+       System.out.print(num*2);
+       System.out.print(' ');
+       System.out.println(num*3);
+    }
+}
+
+
+
+
+
