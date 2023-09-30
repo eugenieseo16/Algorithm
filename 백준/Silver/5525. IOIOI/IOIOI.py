9ff25b0de20bc408c7eb302bfc3239a3ae586d61
Eugene <109324479+eugenieseo16@users.noreply.github.com>
2023-09-30 22:52:57 +0900

    [Silver I] Title: IOIOI, Time: 340 ms, Memory: 33460 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -7,11 +7,19 @@ N = int(input())
 P = "I" + (("OI") * N)
 M = int(input())
 S = input()
+
 idx = 0
 cnt = 0
-L = len(P)
-while idx <= M - L:
-    if S[idx : idx + L] == P:
+ans = 0
+
+while idx <= M - 2:
+    if S[idx : idx + 3] == "IOI":
         cnt += 1
-    idx += 1
-print(cnt)
+        idx += 2
+        if cnt == N:
+            ans += 1
+            cnt -= 1
+    else:
+        cnt = 0
+        idx += 1
+print(ans)
