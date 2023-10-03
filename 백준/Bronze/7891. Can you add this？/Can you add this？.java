e180cbfa890094ea9ad3b27661c60908f7184887
Eugene <109324479+eugenieseo16@users.noreply.github.com>
2023-10-03 20:16:19 +0900

    [Bronze V] Title: Can you add this?, Time: 224 ms, Memory: 17872 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+    
+        int line = sc.nextInt();
+    
+        while( line --> 0){
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            System.out.println(num1 + num2);
+    
+        }
+    }
+}
