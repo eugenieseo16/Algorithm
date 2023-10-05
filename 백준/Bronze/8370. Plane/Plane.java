d456c05700cd7836e934573bc74bc9bf33ae06c0
Eugene <109324479+eugenieseo16@users.noreply.github.com>
2023-10-05 23:29:13 +0900

    [Bronze V] Title: Plane, Time: 208 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int k1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int k2 = sc.nextInt();
+        System.out.println(n1*k1 + n2*k2);
+    }
+}
