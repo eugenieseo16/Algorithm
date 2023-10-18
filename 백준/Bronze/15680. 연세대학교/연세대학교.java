1ac330e85f7fe91cfc59ff9748c3cc461ed047ae
Eugene <109324479+eugenieseo16@users.noreply.github.com>
2023-10-19 02:29:35 +0900

    [Bronze V] Title: 연세대학교, Time: 212 ms, Memory: 17604 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        if(num == 1){
+            System.out.println("Leading the Way to the Future");
+        }else{
+            System.out.println("YONSEI");
+        }
+
+    }
+}
