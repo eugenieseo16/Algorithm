92de144179e683b509c5118a658cc2b1a0945c4d
Eugene <109324479+eugenieseo16@users.noreply.github.com>
2023-10-19 02:37:13 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 232 ms, Memory: 18288 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String id = sc.nextLine();
+        
+        System.out.println(":fan::fan::fan:\n" + ":fan::" + id + "::fan:\n" +":fan::fan::fan:" );
+    }
+}
