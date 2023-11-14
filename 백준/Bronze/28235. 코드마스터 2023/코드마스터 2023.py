be1d19835ed897b130987fced6fc07621fbe63b0
Eugene <109324479+eugenieseo16@users.noreply.github.com>
2023-11-15 00:50:09 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,11 @@
+word = input()
+
+if word == "SONGDO":
+    print("HIGHSCHOOL")
+elif word == "CODE":
+    print("MASTER")
+elif word == "2023":
+    print("0611")
+else:
+    print("CONTEST")
+    
